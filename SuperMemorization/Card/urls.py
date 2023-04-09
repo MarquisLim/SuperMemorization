777761e13d0dae8b78c8a1c7fb0ef954f4736465
DarthZiel <Studerzhan@gmail.com>
2023-04-09 17:32:01 +0600

    update urls

--- SuperMemorization/Card/urls.py
+++ SuperMemorization/Card/urls.py
@@ -5,12 +5,12 @@ from django.http import HttpResponse
 
 urlpatterns = [
     path('', lambda request: HttpResponse('Это апи'), name='home'),
-    path('cards/', CardAPIView.as_view()),  # список и создание карточек
-    path('decks/', DeckAPIView.as_view()),  # список и создание колод 
-    path('deck/<int:id>/', DeckDetailAPIView.as_view()),  # patch и delete колоды
-    path('card/<int:id>/', CardDetailAPIView.as_view()),  # patch и delete карточки
-    path('user/decks/', DecksAPIView.as_view()),
-    path('user/answer/<int:id>', AnswerToCard.as_view()),
+    path('api/v1/cards/', CardAPIView.as_view()),  # список и создание карточек
+    path('api/v1/decks/', DeckAPIView.as_view()),  # список и создание колод
+    path('api/v1/deck/<int:id>/', DeckDetailAPIView.as_view()),  # patch и delete колоды
+    path('api/v1/card/<int:id>/', CardDetailAPIView.as_view()),  # patch и delete карточки
+    path('ap1/v1/user/decks/', DecksAPIView.as_view()),
+    path('api/v1/user/answer/<int:id>', AnswerToCard.as_view()),
     path('api/v1/auth/', include('djoser.urls')),
     re_path('auth/', include('djoser.urls.authtoken')),
     path('api/token/', views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
