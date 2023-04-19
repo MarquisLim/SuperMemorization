a4bde19d66ae53687b0eb34fbfcb79bde6131fe0
DarthZiel <Studerzhan@gmail.com>
2023-04-19 18:33:35 +0600

    sfe

--- SuperMemorization/Card/views.py
+++ SuperMemorization/Card/views.py
@@ -102,7 +102,7 @@ class RegisterAPIView(APIView):
         user = RegisterSerializer(data=request.data)
         if user.is_valid():
             user.save()
-        return Response(user.data, 'Юзер создан')
+        return Response(user.data)
 
 
 
