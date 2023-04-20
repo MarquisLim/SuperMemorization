4e5c6c3466a4b185345a890fa0fa2f25986b70d7
DarthZiel <Studerzhan@gmail.com>
2023-04-20 10:57:24 +0600

    da

--- SuperMemorization/SuperMemorization/settings.py
+++ SuperMemorization/SuperMemorization/settings.py
@@ -108,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
+AUTH_USER_MODEL = 'auth.User'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
