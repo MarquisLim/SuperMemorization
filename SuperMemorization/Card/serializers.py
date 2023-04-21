5d1a96fda2a66601e29cb3a07f17de85a893c96a
DarthZiel <Studerzhan@gmail.com>
2023-04-21 11:50:56 +0600

    dw

--- SuperMemorization/Card/serializers.py
+++ SuperMemorization/Card/serializers.py
@@ -4,39 +4,31 @@ from datetime import date
 
 
 class CurrentCardSerialzier(serializers.ModelSerializer):
-    image_url = serializers.SerializerMethodField()
+   # image_url = serializers.SerializerMethodField()
 
     class Meta:
         model = Card
-        fields = ['front', 'back', 'image_url', 'deck_id', 'ef', 'interval', 'last_review_date', 'next_review_date',
-                  'image']
+        fields = ['front', 'back', 'deck_id', 'ef', 'interval', 'last_review_date', 'next_review_date', 'image']
 
-    def get_image_url(self, obj):
+
+    def to_representation(self, instance):
+        if instance.next_review_date >= date.today():
+            return super().to_representation(instance)
+        return None
+"""   def get_image_url(self, obj):
         request = self.context.get('request')
         if request and obj.image:
             return request.build_absolute_uri(obj.image.url)
         return None
+"""
 
-    def to_representation(self, instance):
-        if instance.next_review_date <= date.today():
-            return super().to_representation(instance)
-        return None
 
 
 class CardSerializer(serializers.ModelSerializer):
-    image_url = serializers.SerializerMethodField()
 
     class Meta:
         model = Card
-        fields = ['front', 'back', 'image_url', 'deck_id', 'ef', 'interval', 'last_review_date', 'next_review_date',
-                  'image']
-
-    def get_image_url(self, obj):
-        request = self.context.get('request')
-        if request and obj.image:
-            print(request.build_absolute_uri(obj.image.url))
-            return request.build_absolute_uri(obj.image.url)
-        return None
+        fields = ['front', 'back', 'deck_id', 'ef', 'interval', 'last_review_date', 'next_review_date', 'image']
 
 
 class DeckSerializer(serializers.ModelSerializer):
