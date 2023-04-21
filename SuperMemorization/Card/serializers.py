456eff69064c9c032d8eeb399e56785b14955232
DarthZiel <Studerzhan@gmail.com>
2023-04-21 11:55:52 +0600

    da

--- SuperMemorization/Card/serializers.py
+++ SuperMemorization/Card/serializers.py
@@ -4,7 +4,7 @@ from datetime import date
 
 
 class CurrentCardSerialzier(serializers.ModelSerializer):
-   # image_url = serializers.SerializerMethodField()
+    image_url = serializers.SerializerMethodField()
 
     class Meta:
         model = Card
@@ -15,12 +15,12 @@ class CurrentCardSerialzier(serializers.ModelSerializer):
         if instance.next_review_date <= date.today():
             return super().to_representation(instance)
         return None
-"""   def get_image_url(self, obj):
+    def get_image_url(self, obj):
         request = self.context.get('request')
         if request and obj.image:
             return request.build_absolute_uri(obj.image.url)
         return None
-"""
+
 
 
 
