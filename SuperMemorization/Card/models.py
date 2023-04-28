07781f465cc0b9171ea79a62a3fbbafd5536a511
DarthZiel <Studerzhan@gmail.com>
2023-04-28 11:20:29 +0600

    dw

--- SuperMemorization/Card/models.py
+++ SuperMemorization/Card/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 class Deck(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True)
-    user_id = models.ForeignKey(User, on_delete=models.PROTECT, null=True, related_name='decks')
+    user_id = models.ForeignKey(User, on_delete=models.PROTECT, related_name='decks')
 
     def __str__(self):
         return self.title
