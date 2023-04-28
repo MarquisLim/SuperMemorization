9a8a4536af3df295b1752e08b93ed10d387f1ac0
DarthZiel <Studerzhan@gmail.com>
2023-04-28 11:52:17 +0600

    dw

--- SuperMemorization/Card/views.py
+++ SuperMemorization/Card/views.py
@@ -34,13 +34,12 @@ class DeckListCreateView(ListCreateAPIView):
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer, request.user)
+        self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
-    def perform_create(self, serializer, user):
-        serializer.save(user=user)
-
+    def perform_create(self, serializer):
+        serializer.save(user_id=self.request.user.id)
 class DeckDetailAPIView(APIView):
 
     def get(self, request, id):
