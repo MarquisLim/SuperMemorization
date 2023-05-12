afc9a2f4e9767d38088691f7051b1ba5b5d8f3ce
DarthZiel <Studerzhan@gmail.com>
2023-05-12 08:50:11 +0600

    deef

--- SuperMemorization/Card/models.py
+++ SuperMemorization/Card/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 class Deck(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True)
-    user_id = models.ForeignKey(User, on_delete=models.PROTECT, related_name='decks')
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='decks')
 
     def __str__(self):
         return self.title
@@ -20,7 +20,7 @@ class Card(models.Model):
     image = models.ImageField(upload_to='IMG', blank=True)
     last_review_date = models.DateField(default=datetime.now)
     next_review_date = models.DateField(default=datetime.now)
-    deck_id = models.ForeignKey('Deck', on_delete=models.PROTECT, related_name='cards')
+    deck_id = models.ForeignKey('Deck', on_delete=models.CASCADE, related_name='cards')
 
     def __str__(self):
         return self.front
